c3a71255dfb15828ffd9648da4ff0b7544f73ad1
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-02-20 19:28:37 -0400

    feat: set authorization header in middleware

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginCard } from "./_components/login-card";
 
 export default function LoginPage() {
   return (
-    <main className="flex min-h-screen w-full items-center justify-center px-3 bg-black">
+    <main className="flex min-h-screen w-full items-center justify-center px-3 bg-slate-800">
       <LoginCard />
     </main>
   );
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export async function middleware(req: NextRequest) {
-  return NextResponse.redirect(new URL("/login", req.url));
+  const token = cookies().get("token");
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  } else {
+    const next = NextResponse.next();
+    next.headers.set("Authorization", `Bearer ${token}`);
+    return next;
+  }
 }
 
 export const config = {
